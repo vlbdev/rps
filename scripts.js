aeede25b40113b5e095255555fc23b5eef4bb4ab
VladimirB <vlbelevich@yandex.by>
2024-11-12 20:29:41 +0300

    Add getComputerChoice function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+function getComputerChoice() {
+    let randomNumber = Math.random();
+    if (randomNumber <= 0.33) {
+        return "Rock";
+    } else if (randomNumber <= 0.66) {
+        return "Paper";    
+    } else {
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
